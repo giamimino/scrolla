7da502b94bfe9ee63e1374a666ad69eae4370b55
giamimino <miminoshvili2102@example.com>
2025-07-19 23:50:30 +0400

    db issues fixed and porofile page development started

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,39 +2,51 @@
 import React, { useEffect, useState } from 'react'
 import styles from './page.module.scss'
 import { useRouter } from 'next/navigation'
+import { Icon } from '@iconify/react'
 
 type User = {
   name: string,
   email: string,
 }
 
-export default function page() {
+export default function Page() {
   const [user, setUser] = useState<User | null>(null)
   const router = useRouter()
 
   useEffect(() => {
     fetch("/api/getUser")
-    .then(res => res.json())
-    .then(data => {
-      console.log("data",data);
-      
-      if(data.success) {
-        setUser(data.user)
-      }
-    })
-    .catch(err => {
-      console.log(err);
-    })
+      .then(res => res.json())
+      .then(data => {
+        console.log("data", data)
+        if (data.success) {
+          setUser(data.user)
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }, [router])
 
   return (
     <div className={styles.page}>
       <div>
-        <h1>
-        Profile
-        </h1>
-        {user?.name && <p>{user?.name}</p>}
-        {user?.email && <p>{user?.email}</p>}
+        <Icon icon={"fa6-solid:user"} />
+        <div>
+          <div>
+            <h2>{user?.name}</h2>
+            <p>{user?.email}</p>
+          </div>
+          <div>
+            <button data-edit>Edit profile</button>
+            <button data-setshare><Icon icon={"material-symbols:settings-rounded"} /></button>
+            <button data-setshare><Icon icon={"majesticons:share"} /></button>
+          </div>
+          <div>
+            <div><span></span><button data-socstats>Following</button></div>
+            <div><span></span><button data-socstats>Followers</button></div>
+            <div><span></span><button data-socstats>Likes</button></div>
+          </div>
+        </div>
       </div>
     </div>
   )
